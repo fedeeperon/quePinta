23292ca974e33e89dc5e9c223143faae418a4d2c
Fede Peron <peronfedee@gmail.com>
2024-10-23 17:16:45 -0300

    corección del register

--- quepinta/quepinta/urls.py
+++ quepinta/quepinta/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 
-    path('login/', views.user_login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
